57ee81db4b3a15f48b3477d9f4503d0806b5fd11
samuelevi87 <samuel.levi.alves@outlook.com>
2022-06-13 12:05:42 -0300

    Iniciando os estudos

---
+++ src/main/java/com/sl3v1/levifoodapi/LevifoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.sl3v1.levifoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevifoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevifoodApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sl3v1/levifoodapi/LevifoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl3v1.levifoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevifoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
