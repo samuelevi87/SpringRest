3051510281e8bb73d3e4175595e37ede64eadb1e
samuelevi87 <samuel.levi.alves@outlook.com>
2022-07-25 08:18:38 -0300

    Modificando o comportamento da aplicação com Spring Profile - @Profile

--- src/main/java/com/sl3v1/levifoodapi/di/notificacao/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/sl3v1/levifoodapi/di/notificacao/notificacao/NotificadorEmailMock.java
@@ -5,18 +5,18 @@ import com.sl3v1.levifoodapi.di.modelo.Cliente;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 
-    public NotificadorEmail(){
-        System.out.println("NotificadorEmail REAL");
+    public NotificadorEmailMock(){
+        System.out.println("NotificadorEmail MOCK");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 
 }
