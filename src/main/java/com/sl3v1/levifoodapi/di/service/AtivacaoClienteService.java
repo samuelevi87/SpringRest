65a77e06b954707ea0babeccae2c27bdefc4c386
samuelevi87 <samuel.levi.alves@outlook.com>
2022-07-25 08:34:04 -0300

    Criando métodos de callback do ciclo de vida dos beans, @PostConstruct, @PreDestroy

--- src/main/java/com/sl3v1/levifoodapi/di/service/AtivacaoClienteService.java
+++ src/main/java/com/sl3v1/levifoodapi/di/service/AtivacaoClienteService.java
@@ -9,12 +9,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class AtivacaoClienteService {
 
     @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
     private Notificador notificador;
+
+    @PostConstruct //@PostConstruct indica para o Spring que um método é executado após a construção do objeto que será executado. Essa anotação faz poarte da especificação da JSR-250, mas o Spring a utiliza.
+    public void init(){
+        System.out.println("INIT " + notificador);
+    }
+
+    @PreDestroy //@PreDestroy indica para o Spring que um método é executado antes da destruição do objeto que será destruído.
+    public void destroy(){
+        System.out.println("DESTROY");
+    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
