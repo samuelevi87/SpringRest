17cec5ddd12fd9ff811260c249daf5fa63c724d8
samuelevi87 <samuel.levi.alves@outlook.com>
2022-08-17 08:53:50 -0300

    JPA/Hibernate - Projeto/Configurações iniciais.

--- src/main/java/com/sl3v1/levifoodapi/di/notificacao/NivelUrgencia.java
+++
@@ -1,5 +0,0 @@
-package com.sl3v1.levifoodapi.di.notificacao;
-
-public enum NivelUrgencia {
-    SEM_URGENCIA, MEDIO, ALTO
-}
--- src/main/java/com/sl3v1/levifoodapi/di/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package com.sl3v1.levifoodapi.di.notificacao;
-
-import com.sl3v1.levifoodapi.di.modelo.Cliente;
-
-public interface Notificador {
-    void notificar(Cliente cliente, String mensagem);
-}
