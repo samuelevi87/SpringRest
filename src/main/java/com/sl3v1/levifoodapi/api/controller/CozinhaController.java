abe050fd39aa524dfe361336393d9ce38fe8412d
samuelevi87 <samuel.levi.alves@outlook.com>
2022-08-26 08:37:47 -0300

    JPA/Hibernate - Singleton Resource.

--- src/main/java/com/sl3v1/levifoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/sl3v1/levifoodapi/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.sl3v1.levifoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +23,8 @@ public class CozinhaController {
         return repository.listarTodas();
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public List<Cozinha> listar2() {
-        return repository.listarTodas();
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id){
+        return repository.buscarPorId(id);
     }
 }
