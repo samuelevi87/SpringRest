51cc7b263ee311438e7780b3e799960536f7e132
samuelevi87 <samuel.levi.alves@outlook.com>
2022-09-04 19:16:09 -0300

    Rest - MAplicando o erro 404 para singleton resorce não encontrado.

--- src/main/java/com/sl3v1/levifoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/sl3v1/levifoodapi/api/controller/CozinhaController.java
@@ -4,13 +4,13 @@ import com.sl3v1.levifoodapi.api.model.CozinhasXmlWrapper;
 import com.sl3v1.levifoodapi.domain.model.Cozinha;
 import com.sl3v1.levifoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/cozinhas")
@@ -31,15 +31,12 @@ public class CozinhaController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{cozinhaId}")
-    public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id){
-//        return ResponseEntity.status(HttpStatus.OK).body(repository.buscarPorId(id));
-//        return ResponseEntity.status(HttpStatus.OK).build();
-//        return ResponseEntity.ok(repository.buscarPorId(id));
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
-        return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .headers(headers)
-                .build();
+    public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
+        Cozinha cozinhaProcurada = repository.buscarPorId(id);
+
+        if (Objects.isNull(cozinhaProcurada)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cozinhaProcurada);
     }
 }
